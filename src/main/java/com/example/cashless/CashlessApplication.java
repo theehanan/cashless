1dc18cb5e5108dfbc0f7c18112a9cc254ac10458
theehanan <hanantahir813@gmail.com>
2023-09-20 11:13:05 +0500

    First Commit

---
+++ src/main/java/com/example/cashless/CashlessApplication.java
@@ -0,0 +1,13 @@
+package com.example.cashless;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CashlessApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CashlessApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/cashless/CashlessApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cashless;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CashlessApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
